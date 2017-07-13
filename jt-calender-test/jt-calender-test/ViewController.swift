9b8ad39f1977300683057f1df780a96555faa62f
Leighroy <leigh@dice.fm>
2017-07-13 10:02:30 +0100

    :sparkles: init

---
+++ jt-calender-test/jt-calender-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jt-calender-test
+//
+//  Created by Leighroy on 13/07/2017.
+//  Copyright © 2017 DICE FM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
